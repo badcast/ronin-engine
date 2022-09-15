5fdb7bb8392689fe92d688e27d8bbfc24fc5fb29
badcast <badcast@github.com>
2022-09-15 23:40:00 +0600

    Change hierarchy

--- src/ronin/Renderer.cpp
+++
@@ -1,8 +0,0 @@
-#include "framework.h"
-#include "Renderer.h"
-namespace RoninEngine::Runtime
-{
-	Renderer::Renderer() :Renderer("Renderer Component"){}
-
-    Renderer::Renderer(const std::string& name) : Component(name) {}
-}
--- src/ronin/SystemInformation.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "dependency.h"
-
-const std::size_t get_process_privateMemory();
-
-const std::size_t get_process_sizeMemory();
